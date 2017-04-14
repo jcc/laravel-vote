e44e3c65e641a7e59dfee66b28689c950b9c36f8
jcc <changejian@gmail.com>
2017-04-14 09:40:35 +0800

    Fixed the class name

--- database/migrations/create_votes_table.php
+++ database/migrations/create_votes_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFollowersTable extends Migration
+class CreateVotesTable extends Migration
 {
     /**
      * Run the migrations.
